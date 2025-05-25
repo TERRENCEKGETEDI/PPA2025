80842248d07d05f07572303840ee2a3bd1af8e02
Terrence Kgetedi <terrencekgetedi@gmail.com>
2025-05-25 16:46:18 +0200

    More Questions

--- static/html/js/multiple.js
+++ static/html/js/multiple.js
@@ -13,14 +13,20 @@ function run(){
         }
     }
 
-    let total=0
+    let total=0;
+    let correctIncorrect="";
     for(let i=0;i<answers.length;i++){
         if(parseInt(answers[i])===parseInt(user[i])){
             total++;
+            correctIncorrect+="Question "+(i+1)+" is correct<br><br>";
+        }
+        else{
+            correctIncorrect+="Question "+(i+1)+" is incorrect. <br>Your answer: "+user[i]+"<br>Correct answer(s): "+answers[i]+"<br><br>";
         }
     }
-    alert(total+"/"+answers.length+" "+parseInt(total*100/answers.length)+"%");
-
+    alert(total+"/"+answers.length+" "+parseInt(total*100/answers.length)+"%\n\nCheck the answers below.");
+    document.getElementById("result").innerHTML=correctIncorrect;
+    document.getElementById("result").style.display="block";
 }
 function submit(){
     let questions=['q1','q2','q3','q4','q5','q6','q7','q8','q9','q10','q11','q12','q13','q14','q15','q16','q17','q18','q19','q20','q21','q22','q23','q24','q25','q26','q27','q28','q29','q30','q31'];
@@ -33,10 +39,10 @@ function submit(){
                 valid=false;
             }
         }
-        if(valid){
-            alert("Please answer all questions");
-            return;
-        }
+        // if(valid){
+        //     alert("Please answer all questions");
+        //     return;
+        // }
     }
     run();
 }
